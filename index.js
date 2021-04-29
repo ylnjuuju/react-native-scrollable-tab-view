39c441e06e857d73e864794a2abde33d0c16baba
coding <940252541@qq.com>
2021-04-29 15:02:11 +0800

    - ViewPagerAndroid 引用源修改

--- index.js
+++ index.js
@@ -10,11 +10,11 @@ const {
   ScrollView,
   Platform,
   StyleSheet,
-  ViewPagerAndroid,
   InteractionManager,
 } = ReactNative;
 const TimerMixin = require('react-timer-mixin');
 
+const ViewPagerAndroid = require('@react-native-community/viewpager')
 const SceneComponent = require('./SceneComponent');
 const DefaultTabBar = require('./DefaultTabBar');
 const ScrollableTabBar = require('./ScrollableTabBar');
